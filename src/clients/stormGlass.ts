b4e8ebe4686c6f1ca1ab70b38289fb158e5ea01c
danilo <danilo>
2021-06-12 14:48:29 -0300

    Tratamento de erros api StormGlass

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,5 @@
 import { AxiosStatic } from "axios";
+import { InternalError } from '@src/util/errors/internal-error';
 
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -30,6 +31,26 @@ export interface ForecastPoint {
     windSpeed: number;
 }
 
+/**
+ * This error type is used when something breaks before the request reaches out to the StormGlass API
+ * eg: Network error, or request validation error
+ */
+export class ClientRequestError extends InternalError {
+    constructor(message: string) {
+        const internalMessage =
+            'Unexpected error when trying to communicate to StormGlass';
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
+export class StormGlassResponseError extends InternalError {
+    constructor(message: string) {
+        const internalMessage =
+            'Unexpected error returned by the StormGlass service';
+        super(`${internalMessage}: ${message}`);
+    }
+}
+
 export class StormGlass {
 
     readonly stormGlassApiPrams = "swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed";
@@ -38,9 +59,29 @@ export class StormGlass {
     constructor(protected request: AxiosStatic) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-        const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?source=${this.stormGlassAPISource}&params=${this.stormGlassApiPrams}&lat=${lat}&lng=${lng}&key=`)
 
-        return this.normalizeResponse(response.data);
+        try {
+            const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?source=${this.stormGlassAPISource}&params=${this.stormGlassApiPrams}&lat=${lat}&lng=${lng}&key=`,
+                {
+                    headers: {
+                        Authorization: 'fake-token',
+                    },
+                }
+            );
+
+            return this.normalizeResponse(response.data);
+        } catch (err) {
+            /**
+      * This is handling the Axios errors specifically
+      */
+            if (err.response && err.response.status) {
+                throw new StormGlassResponseError(
+                    `Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`
+                );
+            }
+
+            throw new ClientRequestError(err.message);
+        }
     }
 
     private normalizeResponse(
